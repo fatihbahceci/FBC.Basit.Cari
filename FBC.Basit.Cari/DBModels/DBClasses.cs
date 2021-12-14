de1322bec5d701787214233c2415401bd319c9a6
Fatih B <fatihbahceci@gmail.com>
2021-12-14 06:00:13 +0300

    initial files

---
+++ FBC.Basit.Cari/DBModels/DBClasses.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+
+/// <summary>
+/// https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
+/// </summary>
+namespace FBC.Basit.Cari.DBModels
+{
+    public class CariKart
+    {
+        public int CariKartId { get; set; }
+
+        public string Isim { get; set; }
+        public List<CariHareket> Hareketler { get; set; }
+    }
+
+    public class CariHareket
+    {
+        public int CariHareketId { get; set; }
+        public DateTime? Tarih { get; set; }
+        public string? Aciklama { get; set; }
+        public decimal Borc { get; set; }
+        public decimal Alacak { get; set; }
+        public DateTime? VadeTarihi { get; set; }
+
+        public int CariKartId { get; set; }
+        public CariKart CariKart { get; set; }
+    }
+    
+
+    public class DB : DbContext
+    {
+        public DbSet<CariKart> CariKart { get; set; }
+        public DbSet<CariHareket> CariHareket { get; set; }
+
+        public string DbPath { get; }
+
+        public DB()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "basitcari.db");
+        }
+        static DB()
+        {
+            try
+            {
+                using (var db = new DB())
+                {
+                    db.Database.Migrate();
+                }
+            } catch
+            {
+
+            }
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+    public static class TypeHelper
+    {
+        public static string GetEnumDescription(this Enum? enumObj)
+        {
+            if (enumObj == null)
+            {
+                return "Null";
+            }
+            else
+            {
+                FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
+
+                object[] attribArray = fieldInfo.GetCustomAttributes(false);
+
+                if (attribArray.Length == 0)
+                {
+                    return enumObj.ToString();
+                }
+                else
+                {
+                    DescriptionAttribute attrib = attribArray[0] as DescriptionAttribute;
+                    return attrib.Description;
+                }
+            }
+        }
+
+    }
+}
---
+++ FBC.Basit.Cari/Migrations/20211214025143_first.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FBC.Basit.Cari.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CariKart",
+                columns: table => new
+                {
+                    CariKartId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Isim = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CariKart", x => x.CariKartId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CariHareket",
+                columns: table => new
+                {
+                    CariHareketId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Tarih = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Aciklama = table.Column<string>(type: "TEXT", nullable: true),
+                    Borc = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Alacak = table.Column<decimal>(type: "TEXT", nullable: false),
+                    VadeTarihi = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CariKartId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CariHareket", x => x.CariHareketId);
+                    table.ForeignKey(
+                        name: "FK_CariHareket_CariKart_CariKartId",
+                        column: x => x.CariKartId,
+                        principalTable: "CariKart",
+                        principalColumn: "CariKartId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CariHareket_CariKartId",
+                table: "CariHareket",
+                column: "CariKartId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CariHareket");
+
+            migrationBuilder.DropTable(
+                name: "CariKart");
+        }
+    }
+}
