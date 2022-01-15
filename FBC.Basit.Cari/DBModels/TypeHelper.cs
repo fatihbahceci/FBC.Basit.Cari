14cccbe4890a3059203418bed18e87d06419c14e
Fatih B <fatihbahceci@gmail.com>
2022-01-16 01:23:05 +0300

    ****************************** FBC.Basit.Cari ****************************** 2022.01.16-0.0.1.1-[FatihB]     [EN] - Added version info to project

--- FBC.Basit.Cari/DBModels/TypeHelper.cs
+++ FBC.Basit.Cari/DBModels/TypeHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Reflection;
+using System.Text;
 
 /// <summary>
 /// https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
@@ -31,6 +32,38 @@ namespace FBC.Basit.Cari.DBModels
                 }
             }
         }
+        static string versionInfo = null;
+        public static string GetAppVersionInfo()
+        {
+            if (versionInfo == null)
+            {
+                StringBuilder versionInfoSB = new StringBuilder("Version: ");
+                try
+                {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                    versionInfoSB.Append(Assembly.GetExecutingAssembly()
+                                                    .GetName()
+                                                    .Version
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                                                    .ToString());
+                }
+                catch
+                {
+                    versionInfoSB.Append("?.?.?.?");
+                }
+                try
+                {
+                    DateTime buildDate = new FileInfo(Assembly.GetExecutingAssembly().Location).LastWriteTime;
+                    versionInfoSB.Append(" Build: " + buildDate.ToString("yyyy-MM-dd HH:mm:ss"));
+                }
+                catch
+                {
+                    versionInfoSB.Append(" Build: " + "UNKNOWN");
+                }
+                versionInfo = versionInfoSB.ToString();
+            }
+            return versionInfo;
 
+        }
     }
 }
