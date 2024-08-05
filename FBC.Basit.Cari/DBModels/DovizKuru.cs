ec30f3a0777f6da37baf9d3c47a471b911953fe1
Fatih Bahceci <fatih.bahceci@nanodems.com>
2024-08-06 02:31:06 +0300

    2024.08.06-0.0.3.10-[FatihB]     [EN] Döviz kuru yönetimi

--- FBC.Basit.Cari/DBModels/DovizKuru.cs
+++ FBC.Basit.Cari/DBModels/DovizKuru.cs
@@ -6,10 +6,32 @@ namespace FBC.Basit.Cari.DBModels
     {
         [Key]
         public int DovizKuruId { get; set; }
+        /// <summary>
+        /// TRY, USD, EUR, GBP, JPY, CNY, RUB, ...
+        /// </summary>
         public string DovizCinsi { get; set; }
+        /// <summary>
+        /// Türk Lirası, Amerikan Doları, Euro, İngiliz Sterlini, Japon Yeni, Çin Yuanı, Rus Rublesi, ...
+        /// </summary>
         public string DovizAdi { get; set; }
         public decimal GuncelKurAlis { get; set; }
         public decimal GuncelKurSatis { get; set; }
         public DateTime GuncellemeTarihi { get; set; }
+
+        public DovizKuru()
+        {
+            DovizCinsi = "XXX";
+            DovizAdi = "XXX Lirası";
+            GuncellemeTarihi = DateTime.Now;
+        }
+
+        public void Fill(DovizKuru dovizKuru)
+        {
+            DovizCinsi = dovizKuru.DovizCinsi;
+            DovizAdi = dovizKuru.DovizAdi;
+            GuncelKurAlis = dovizKuru.GuncelKurAlis;
+            GuncelKurSatis = dovizKuru.GuncelKurSatis;
+            GuncellemeTarihi = dovizKuru.GuncellemeTarihi;
+        }
     }
 }
